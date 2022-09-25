91b6e20fa4221232010c69c364dae75c81f0f0d0
bubuq3 <62507500+bubuq3@users.noreply.github.com>
2022-09-25 12:35:35 +0100

    added theme config and types for it

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../lib/styles";
 
 export const App = () => {
-  return <div>Hello worlds!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContainer>Hello worlds!</AppContainer>
+    </ThemeProvider>
+  );
 };
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,12 @@
+import { AppTheme } from "./types";
+
+export const theme: AppTheme = {
+  colors: {
+    primary: "#3498db",
+    background: "#282728",
+    foreground: "#484748",
+    error: "#44569",
+    typography: "#c8d6e5",
+    input: "#484748",
+  },
+};
